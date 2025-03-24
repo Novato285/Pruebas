14463c60cdb4d76254c457e2a8967126396ae533
brenda.hermosillo <brenda.hermosillo@upnay.edu.mx>
2025-03-24 15:09:23 +0000

    BASE DE DATOS

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/core/app_colors.dart';
+import 'package:myapp/firebase_options.dart';
 import 'package:myapp/screens/login_screen.dart';
 import 'package:myapp/screens/register_screen.dart';
 
-void main() {
+Future<void> main() async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const ABCondominios());
 }
 
@@ -18,4 +21,3 @@ class ABCondominios extends StatelessWidget {
     );
   }
 }
-
